4a9c94b1fd0fb99eb42c5ccf46f97da18e386a83
vaibhav19mark1 <54894553+vaibhav19mark1@users.noreply.github.com>
2020-10-04 00:28:47 +0530

    Update and rename Q26_Recurrsion_Sumofdigits.cpp to Q26_Sum_of_digits_using_recursion.cpp
    This c++ program calculates the sum of digits of a number using a recursive function.

--- Q26_Sum_of_digits_using_recursion.cpp
+++ Q26_Sum_of_digits_using_recursion.cpp
@@ -1,7 +1,10 @@
+//Program to compute sum of digits of a number
+
 #include<iostream>
 using namespace std;
+// Recurrive function for calculation
 int sumofdigits(int n){
-    if(n==0)
+    if(n==0)         // break condition
     return 0;
     return (n%10 + sumofdigits(n/10));
 }
@@ -9,7 +12,7 @@ int main(){
     int num;
     cout<<"Enter number: ";
     cin>>num;
-    int s=sumofdigits(num);
+    int s=sumofdigits(num); // function call
     cout<<"Sum of digits of number "<<num<<" is "<<s<<endl;
     return 0;
 }
