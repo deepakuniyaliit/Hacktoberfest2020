8d57f402940a6c8ef14fd07ccf09edd445d7dbff
vaibhav19mark1 <54894553+vaibhav19mark1@users.noreply.github.com>
2020-10-04 00:23:36 +0530

    Update and rename Q20_pattern_diamond.cpp to Q20_print_diamond_pattern.cpp
    This c++ program prints a diamond shaped pattern.

--- Q20_print_diamond_pattern.cpp
+++ Q20_print_diamond_pattern.cpp
@@ -1,9 +1,13 @@
+//Program for printing diamond pattern 
+
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     cout<<"enter number of rows: ";
     cin>>n;
+    // Loops for printing upper half of diamond
+    
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n-i;j++){
             cout<<" ";
@@ -16,6 +20,8 @@ int main(){
         }
         cout<<endl;
     }
+    // Lops for printing lower half of diamond
+    
     for(int i=1;i<n;i++){
         for(int j=1;j<=i;j++){
             cout<<" ";
