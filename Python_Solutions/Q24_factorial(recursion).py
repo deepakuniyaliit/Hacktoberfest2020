dad001701281a58590fd4158176a3fd9c2ad9aff
Kunal Aaryen Sinha <aaryen11@pop-os.localdomain>
2020-10-04 00:05:18 +0530

    Changes Added

--- Python_Solutions/Q24_factorial(recursion).py
+++ Python_Solutions/Q24_factorial(recursion).py
@@ -1,9 +1,17 @@
-def factorial(x):
-    if x == 1:
+#%%
+#Function for calulating the factorial
+def factorial(x): 
+    if x == 1 or x== 0 : #base/terimination condition
         return 1
+
+    elif(x < 0) :
+        print("Sorry, factorial does not exist for negative numbers")
+    
     else:
-        return (x * factorial(x-1))
+        return (x * factorial(x-1)) #Recursive Call for function factorial
 
+# main
+num = int(input("Enter a Number :")) # To take input from the user (should be > = 0)
+print("Factorial of", num, "is", factorial(num)) 
 
-num = int(input("Enter a Number :"))
-print("Factorial of", num, "is", factorial(num))
+# %%
