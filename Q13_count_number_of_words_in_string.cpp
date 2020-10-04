f7cf9b1a2dc2370baa5b85b08723e774971f728f
riya-arora08 <72336481+riya-arora08@users.noreply.github.com>
2020-10-04 18:18:17 +0530

    updated string words

--- Q13_count_number_of_words_in_string.cpp
+++ Q13_count_number_of_words_in_string.cpp
@@ -1,5 +1,29 @@
 //Program to find the no of words in a string 
 
+#include<stdio.h>
+int main()
+{
+      char s[150];
+      int i, word=1;
+
+      printf("Enter a string: ");
+      scanf("%[^\n]",s);
+
+      for(i = 0; s[i] != '\0'; ++i)
+      {
+          if(s[i]==' '&& s[i+1]!=' ') word++;
+      }
+
+      // In input there is many spaces between words e.g "hello     world"
+      if(s[i-1]==' ')word--;
+      if(s[0]==' ')word--;
+      if(s[0]=='\0')word--;
+
+      printf("Word: %d",word);
+      return 0;
+}
+
+/*
 #include<iostream>
 #include<string>
 using namespace std;
