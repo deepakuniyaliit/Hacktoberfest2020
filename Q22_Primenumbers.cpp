b6854a8ae4435f58bfe86c776cda40276478ebaa
Urvashi bali <68771075+Kashvibali696@users.noreply.github.com>
2020-10-05 12:00:59 +0530

    Q22_PRIME NO .cpp

--- Q22_Primenumbers.cpp
+++ Q22_Primenumbers.cpp
@@ -1,39 +1,27 @@
 // C++ program to find the prime numbers between a given interval
 #include<iostream>
 using namespace std;
-
-// Function to print prime number in given range
-void primeInRange(int L, int U)
+int main()
 {
-    int flag;
+int n,i,lower,upper,c;
+ cout<<"Enter the Range : ";
+cin>>lower>>upper;
+
+    cout<<"The list of prime numbers in given range:";
     // Traverse each number in the interval with the help of for loop
-    for (int i=L;i<=U;i++) {
-        // Skip 0 and 1 as they are neither prime nor composite
-        if (i == 1 || i == 0)
-            continue;
-        // flag variable to tell if i is prime or not
-        flag = 1;
-        for (int j=2;j<=i/2;j++) {
-            if (i % j == 0) {
-                flag=0;
+    for ( n=lower;n<=upper;n++) {
+        c=0;
+
+
+        for (i=2;i<=n/2;i++) {
+            if (n % i == 0) {
+                c++;
                 break;
             }
         }
-        // flag = 1 means i is prime
-        // and flag = 0 means i is not prime
-        if (flag == 1)
-            cout << i << " ";
+        if(c==0 && n!=1)
+            cout<<n<<" ";
     }
-}
+    }
 
-// Driver Code
-int main()
-{
-    // Given Range
-    int L,U;
-    cout<<"Enter the Range:";
-    cin>>L>>U;
-    cout<<"The list of prime numbers in given range:";
-    // Function Call
-    primeInRange(L,U);
-}
+
