9ca025b4aa87b9b6f0298c45deaa62ed80321e1e
Urvashi bali <68771075+Kashvibali696@users.noreply.github.com>
2020-10-05 12:19:38 +0530

    update_Q9_Vowel removal.c

--- Q9_Vowel_removal.c
+++ Q9_Vowel_removal.c
@@ -1,26 +1,19 @@
 #include<stdio.h>
 #include<string.h>
-void main()
+int main()
 {
 
 	char str[200];
-	int len, i, j;
+	int len, i;
 	printf("Enter a string : ");
 	gets(str);
-	len=strlen(str);
-	for(i=0; i<len; i++)
+	for(i=0;str[i]!='\0'; i++)
 	{
-		if(str[i]=='a' || str[i]=='e' || str[i]=='i' ||
-		str[i]=='o' || str[i]=='u' || str[i]=='A' ||
-		str[i]=='E' || str[i]=='I' || str[i]=='O' ||
-		str[i]=='U') //condition to check for vowels
-		{
-			for(j=i; j<len; j++)
-			{
-				str[j]=str[j+1]; //logic to shift each character one place backward whenever a vowel is found
-			}
-		len--; //decreasing the length of the string accordingly
-		}
+	if(str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!= 'u' && str[i]!='A' &&  str[i]!='E' && str[i]!='I' && str[i]!='O' && str[i]!='U')
+	{
+	printf("%c", str[i]);
 	}
-	printf("Modified string : %s",str);
+	}
+
 }
+
