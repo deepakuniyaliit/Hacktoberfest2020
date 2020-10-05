220be44019db3d4d14778111670e84112f7de951
Deepak Uniyal <deepakuniyaliit@users.noreply.github.com>
2020-10-05 23:38:36 +0530

    Update and rename Q22_Primenumbers.cpp to Q22_PrimeNumbers.cpp

--- Q22_PrimeNumbers.cpp
+++ Q22_PrimeNumbers.cpp
@@ -3,16 +3,13 @@
 using namespace std;
 int main()
 {
-int n,i,lower,upper,c;
- cout<<"Enter the Range : ";
-cin>>lower>>upper;
-
-    cout<<"The list of prime numbers in given range:";
+  int n,i,lower,upper,c;
+  cout<<"Enter the Range : ";
+  cin>>lower>>upper;
+  cout<<"The list of prime numbers in given range:";
     // Traverse each number in the interval with the help of for loop
     for ( n=lower;n<=upper;n++) {
         c=0;
-
-
         for (i=2;i<=n/2;i++) {
             if (n % i == 0) {
                 c++;
@@ -21,7 +18,7 @@ cin>>lower>>upper;
         }
         if(c==0 && n!=1)
             cout<<n<<" ";
-    }
-    }
+ }
+}
 
 
