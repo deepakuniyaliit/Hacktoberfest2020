a0d3ee71f2e64c293494558688af1a3076c4cee0
Siva Kumar G <61716135+SivaKumar2001@users.noreply.github.com>
2020-10-10 13:56:39 +0530

    Arithmetic Progression.cpp

--- Arithematic Progression.cpp
+++ Arithematic Progression.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
-// for arithematic  progression there will a starting number , common difference and ending number
+// for arithmetic  progression there will a starting number , common difference and ending number
+
 // AP = a+d till n;
+
+//Time Complexity - O(N)
+
 int main()
 {
-    int N; // N number of terms if N is 5 output will be five terms
-    cin >>N;
+    int N;
+    cout<<"Enter the value of N: ";
+    cin>>N;
     int starting_number = 2;
     int common_difference = 2;
     for(int i=0;i<N;i++)
@@ -15,7 +20,5 @@ int main()
         cout <<starting_number<<" ";
         starting_number = starting_number + common_difference;
     }
-
-}
-
+    return 0;
 
