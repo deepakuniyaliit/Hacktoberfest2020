d876eebd7884a793aeabfbd3b797e4730af3959a
Siva Kumar G <61716135+SivaKumar2001@users.noreply.github.com>
2020-10-11 23:03:05 +0530

    Update fibonacci of nth number

--- Fibonacci of nth number.cpp
+++ Fibonacci of nth number.cpp
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-//Fibonacci of nth number - fibonacci sequence starts from 0,1,1,2,3,5.......
-
-//fibonacci sequence - first and second number is equal to third number and second and third number is equal to fourth number ......
-
 //Time Complexity - O(N)
 
 int fib(int N)
@@ -28,6 +24,6 @@ int main()
     int N;
     cout<<"Enter the value of N: ";
     cin>>N;
-    cout<<"Fibonacci number of Nth term is:"<<fib(N);
+    cout<<"Fibonacci number of Nth term is "<<fib(N);
     return 0;
 }
